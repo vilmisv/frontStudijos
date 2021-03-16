032f05f528db63b9cbb4c92ffec7db1b31e34ff8
vilmisv <wilmisv1995@gmail.com>
2021-03-16 22:31:58 +0200

    3 uzduoti funkcijos

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const number = 4;
-let atsakymas;
+// const number = 4;
+// let atsakymas;
 
-do {
-    atsakymas = prompt('spek skaiciu');
-    if (atsakymas < number) {
-        alert('too low')
-    } else if (atsakymas > number) {
-        alert('to high')
-    }
+// do {
+//     atsakymas = prompt('spek skaiciu');
+//     if (atsakymas < number) {
+//         alert('too low')
+//     } else if (atsakymas > number) {
+//         alert('to high')
+//     }
 
-} while (number != atsakymas);
+// } while (number != atsakymas);
 
-
-
-
-alert('atspejai');
+// alert('atspejai');
 
 
+function sayHi (name) {
+    return 'hi ' + name;
+}
+
+
+
+// const kibiras = sayHi('aurimas');
+
+// alert(kibiras);
+
+alert(sayHi('aurimas'))
